1babc8d9e195bf09c3996068a08a0731b9fe4be1
unknown <ushiojun@gmail.com>
2021-07-15 17:12:56 +0800

    revised

--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -17,34 +17,33 @@ const aboutMe = () => {
                   <hr className="w-40 ml2 mr0 mv0"></hr>
                   <p className="bodoni f3 tl mt0 ml0 mb2 mr0 pa2">I'm Benjamin</p>
                   <p className="f5 ma1 pa2">
-                    I graduated with a Bachelor degree in Business Accoutancy (distinction) and also had a diploma in Digital Media. <br />
-                    I create digital contents as a hobby, mainly excel and automation. <br /> 
-                    I am a self-driven, self-taught person and a generalist who constantly seeks to improve my craft.
+                    I graduated with a Bachelor degree in Business Accoutancy (distinction) and had a diploma in Digital Media. <br />
+                    I create digital contents as a hobby, mainly working with Excel and automation programs. <br /> 
+                    I am a self-taught person and a generalist who constantly seeks to improve my craft.
                   </p>
                   <p className="f5 ma1 pa2">
                     I began my career in a tourism organization where I worked as an operations coordinator for 3 years and supported backend 
                     activities for a large operation team. In the last 5 years, I have been doing operations accounting and have experience 
                     in managing both accounts payables and receivables. 
                     In the course of work experience, I have consistently achieved targeted levels of processing 
-                    transactions, maintained constant accuracy in month-end reconciliations and journals postings, and 
-                    collaborated with cross-functional stakeholders to develop informative financial dashboards. Aside with a good 
-                    track record, I have also contributed to various process efficiencies projects and achieved
+                    transactions, maintained timely and accurate month-end reconciliations and journals postings, and 
+                    collaborated with cross-functional stakeholders to develop informative financial dashboards. 
+                    I have also contributed to various process efficiencies projects and achieved
                     positive outcomes and favourable responses from my stakeholders.
                   </p>
                   <p className="f5 ma1 pa2">
                     My goal is to continue to upskill my competencies over the next few years in areas of finance and digital technology.
-                    I do this by habitual reading and strive to nurture a learning environment.
+                    I do this by habitual reading and strive to be a part of a learning environment.
                   </p>
                   <p className="f5 ma1 pa2">
-                    Outside of my day job, I continue learning and exploring new digital technologies. <br />
-                    The following softwares are my key <strong>skills and competencies</strong>:
+                    Outside of my day job, I continue exploring new digital technologies. <br />
+                    The following areas are my key <strong>skills and competencies</strong>:
                     <ol>
-                      <li>SAP ERP, Quickbooks, Microsoft Excel, PowerPoint, Word, Outlook, SharePoint, Visio</li>
-                      <li>Tableau and Power BI</li>
-                      <li>Infowise forms and PowerApps</li>
-                      <li>Python, VBA and UIPath(RPA)</li>
-                      <li>HTML5, CSS3, ECMAScript, React, Node, Express, PostgreSQL</li>
-                      <li>Adobe Premiere Pro and Photoshop</li>
+                      <li>Analytics and Dashboards with Tableau and Power BI</li>
+                      <li>Finance and Accounting with SAP ECC6.0/SAP S/4 Hana and SharePoint</li>
+                      <li>Automation with Python, MS-VBA and UIPath(RPA)</li>
+                      <li>Web Development with HTML5, CSS3, ECMAScript, React, Node, Express, PostgreSQL</li>
+                      <li>Videography/Video editing with Adobe Premiere Pro</li>
                     </ol> 
                   </p>
                   <p className="f5 ma1 pa2">
